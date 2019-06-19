f1d57ecb4dd66a568ebb33ab001f2433f95c8788
subarna <khadka.subarnabijaya@gmail.com>
2019-06-19 01:20:24 -0400

    first commit

---
+++ complexDemo/Complex.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace complexDemo
+{
+    class Complex
+    {
+        public int Real { get; }
+        public int Imaginary { get; }
+        public double Argument {
+            get
+            {
+                return Math.Atan(Imaginary / Real);
+            }
+        }
+        public double Modulus {
+            get
+            {
+                return Math.Sqrt(Math.Pow(Real,2)+Math.Pow(Imaginary,2));
+            }
+        }
+
+
+
+        public Complex(int real = 0, int imaginary = 0)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+        public override string ToString()
+        {
+            return $"<{Real},{Imaginary}>";
+        }
+        public static Complex operator +(Complex lhs, Complex rhs)
+        {
+            int real = lhs.Real + rhs.Real;
+            int imaginary = lhs.Imaginary + rhs.Imaginary;
+            return new Complex(real, imaginary);
+        }
+        public static Complex operator -(Complex lhs, Complex rhs)
+        {
+            int real = lhs.Real + rhs.Real;
+            int imaginary = lhs.Imaginary - rhs.Imaginary;
+            return new Complex(real, imaginary);
+        }
+        public static bool operator ==(Complex lhs, Complex rhs)
+        {
+            return (lhs.Real == rhs.Real) && (lhs.Imaginary == rhs.Imaginary);
+        }
+          
+        public static bool operator !=(Complex lhs, Complex rhs)
+        {
+            return (lhs.Real != rhs.Real) || (lhs.Imaginary != rhs.Imaginary);
+            
+        }
+        public static Complex operator -(Complex complex)
+        {
+            return new Complex(-complex.Real, -complex.Imaginary);
+
+        }
+        public static Complex operator *(Complex lhs, Complex rhs)
+        {
+            //<a, b> * <c, d> = <ac-bd, ad+bc>. 
+            return new Complex((lhs.Real * rhs.Real) - (lhs.Imaginary * rhs.Imaginary), (lhs.Real * rhs.Imaginary) + (lhs.Imaginary * rhs.Real));
+        }
+            
+        
+
+
+    }
+}
---
+++ complexDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace complexDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c0 = new Complex(-2, 3);
+            Complex c1 = new Complex(-2, 3);
+            Complex c2 = new Complex(1, -2);
+            Complex c3 = new Complex(3, 9);
+
+            Console.WriteLine($"{c0}");
+            Console.WriteLine(c1);
+
+            Console.WriteLine($"{c1} + {c2} = {c1 + c2}");
+            Console.WriteLine($"{c1} - {c2} = {c1 - c2}");
+
+            Console.WriteLine($"{c3} in polar form is {c3.Modulus:f2} and inverse of tan is({c3.Argument:f2})");
+           
+            Console.WriteLine($"{c0} {(c0 == c1 ? "=" : "!=")} {c1}");
+            Console.WriteLine($"{c0} {(c0 == c2 ? "=" : "!=")} {c2}");
+            Console.WriteLine($"negating {c3} gives {-c3}");
+            Console.WriteLine($"Multiplying {c0} and {c1} gives {c0*c1}");
+
+            
+
+        }
+    }
+}
